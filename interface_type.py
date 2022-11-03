9696b62eb0f7d788f378866c968ce8e14077a32c
cyy <cyyever@outlook.com>
2022-11-03 15:28:17 +0800

    add code

---
+++ interface_type.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from enum import IntEnum, auto
+
+
+# we use POSIX errno for error notification, see $(man 3 errno) for details
+class Errno(IntEnum):
+    SUCC = auto()
+
+
+# RAID interface types
+@dataclass
+class BlockReadRequest:
+    block_no: int
+
+
+@dataclass
+class BlockWriteRequest:
+    block_no: int
+    block: bytes
+
+
+@dataclass
+class BlockReadReply:
+    errno: Errno
+    block_no: int
+    block: bytes
+
+
+@dataclass
+class BlockWriteReply:
+    errno: Errno
+    block_no: int
