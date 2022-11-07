a3f888f40e01444ba29d9865f15b327b77cb2854
cyy <cyy@outlook.com>
2022-11-07 14:05:44 +0800

    add code

--- config.hpp
+++ config.hpp
@@ -5,46 +5,51 @@
  */
 #pragma once
 #include <filesystem>
-#include <fmt/format.h>
 #include <memory>
 #include <regex>
 #include <string>
+
+#include <fmt/format.h>
+
 #include <yaml-cpp/yaml.h>
 
 namespace raid_fs {
-struct config {
-  explicit config(const std::filesystem::path &config_file) {
-    YAML::Node yaml_node = YAML::LoadFile(config_file);
+  struct config {
+    explicit config(const std::filesystem::path &config_file) {
+      YAML::Node yaml_node = YAML::LoadFile(config_file);
 
-    disk_capacity = parse_size(yaml_node, "disk_capacity");
-  }
-  static size_t parse_size(const YAML::Node &node, const std::string &key) {
-    size_t result = 0;
-    std::smatch match;
-    std::regex re("^([0-9]+)([KG]?)B$");
-    auto config_value = node[key].as<std::string>();
-    if (std::regex_search(config_value, match, re)) {
-      result = std::stoull(match.str(1));
-      if (match.str(2) == "K") {
-        result *= 1024;
-      } else if (match.str(2) == "G") {
-        result *= 1024 * 1024 * 1024;
-      }
-      return result;
+      disk_capacity = parse_size(yaml_node, "disk_capacity");
     }
-    throw std::invalid_argument(
-        fmt::format("invalid value for {}:{}", key, config_value));
-  }
-  size_t disk_capacity{};
-};
-struct filesystem_config : public config {
-  explicit filesystem_config(const std::filesystem::path &config_file)
-      : config(config_file) {
-    YAML::Node yaml_node = YAML::LoadFile(config_file)["filesystem"];
-    port = yaml_node["port"].as<uint16_t>();
-    block_size = parse_size(yaml_node, "block_size");
-  }
-  uint16_t port{};
-  size_t block_size{};
-};
+    static size_t parse_size(const YAML::Node &node, const std::string &key) {
+      size_t result = 0;
+      std::smatch match;
+      std::regex re("^([0-9]+)([KG]?)B$");
+      auto config_value = node[key].as<std::string>();
+      if (std::regex_search(config_value, match, re)) {
+        result = std::stoull(match.str(1));
+        if (match.str(2) == "K") {
+          result *= 1024;
+        } else if (match.str(2) == "G") {
+          result *= 1024 * 1024 * 1024;
+        }
+        return result;
+      }
+      throw std::invalid_argument(
+          fmt::format("invalid value for {}:{}", key, config_value));
+    }
+    size_t disk_capacity{};
+  };
+  struct filesystem_config : public config {
+    explicit filesystem_config(const std::filesystem::path &config_file)
+        : config(config_file) {
+      YAML::Node yaml_node = YAML::LoadFile(config_file)["filesystem"];
+      port = yaml_node["port"].as<uint16_t>();
+      block_size = parse_size(yaml_node, "block_size");
+      if (disk_capacity % block_size != 0) {
+        throw std::invalid_argument("disk can't be partitioned into blocks");
+      }
+    }
+    uint16_t port{};
+    size_t block_size{};
+  };
 } // namespace raid_fs
--- fs_server.cpp
+++ fs_server.cpp
@@ -4,63 +4,63 @@
  * \brief File system implementation
  */
 
+#include <fmt/format.h>
 #include <grpc/grpc.h>
 #include <grpcpp/security/server_credentials.h>
 #include <grpcpp/server.h>
 #include <grpcpp/server_builder.h>
 #include <grpcpp/server_context.h>
-#include <fmt/format.h>
 
 #include "config.hpp"
 #include "fs.grpc.pb.h"
 
 namespace raid_fs {
-struct super_block {
-  char FS_type[8];
-  uint16_t FS_version;
-  uint64_t bitmap_block_offset;
-  uint64_t inode_bitmap_size;
-  uint64_t data_bitmap_size;
-  uint64_t inode_table_offset;
-  uint64_t inode_number;
-  uint64_t data_table_offset;
-  uint64_t data_block_number;
-} __attribute__((aligned(128)));
+  struct super_block {
+    char FS_type[8];
+    uint16_t FS_version;
+    uint64_t bitmap_block_offset;
+    uint64_t inode_bitmap_size;
+    uint64_t data_bitmap_size;
+    uint64_t inode_table_offset;
+    uint64_t inode_number;
+    uint64_t data_table_offset;
+    uint64_t data_block_number;
+  } __attribute__((aligned(128)));
 
-class FS_server final : public raid_fs::FileSystem::Service {
-public:
-  explicit FS_server(size_t disk_capacity_, size_t block_size_)
-      : disk_capacity(disk_capacity_), block_size(block_size_) {
-    if (!read_super_block()) {
-      throw std::runtime_error("failed to read super block");
+  class FS_server final : public raid_fs::FileSystem::Service {
+  public:
+    explicit FS_server(size_t disk_capacity_, size_t block_size_)
+        : disk_capacity(disk_capacity_), block_size(block_size_) {
+      if (!read_super_block()) {
+        throw std::runtime_error("failed to read super block");
+      }
+      // file system is not initialized
+      if (super_blk.FS_type != raid_fs_type) {
+      }
     }
-    // file system is not initialized
-    if (super_blk.FS_type != raid_fs_type) {
+
+  public:
+    static constexpr auto raid_fs_type = "RAIDFS";
+
+  private:
+    bool read_super_block() {
+      super_blk = {.FS_type = "RAIDFS",
+                   .FS_version = 0,
+                   .bitmap_block_offset = 0,
+                   .inode_bitmap_size = 0,
+                   .data_bitmap_size = 0,
+                   .inode_table_offset = 0,
+                   .inode_number = 0,
+                   .data_table_offset = 0,
+                   .data_block_number = 0};
+      return true;
     }
-  }
 
-public:
-  static constexpr auto raid_fs_type = "RAIDFS";
-
-private:
-  bool read_super_block() {
-    super_blk = {.FS_type = "RAIDFS",
-                 .FS_version = 0,
-                 .bitmap_block_offset = 0,
-                 .inode_bitmap_size = 0,
-                 .data_bitmap_size = 0,
-                 .inode_table_offset = 0,
-                 .inode_number = 0,
-                 .data_table_offset = 0,
-                 .data_block_number = 0};
-    return true;
-  }
-
-private:
-  size_t disk_capacity;
-  size_t block_size;
-  super_block super_blk{};
-};
+  private:
+    size_t disk_capacity;
+    size_t block_size;
+    super_block super_blk{};
+  };
 } // namespace raid_fs
 int main(int argc, char **argv) {
   if (argc <= 1) {
@@ -68,7 +68,7 @@ int main(int argc, char **argv) {
     return -1;
   }
   raid_fs::filesystem_config cfg(argv[1]);
-  std::string server_address(fmt::format("0.0.0.0:{}",cfg.port));
+  std::string server_address(fmt::format("0.0.0.0:{}", cfg.port));
 
   raid_fs::FS_server service(cfg.disk_capacity, cfg.block_size);
 
