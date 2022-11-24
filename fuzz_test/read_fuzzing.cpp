abdef4468a88bc4da2fe79f2923a811c68edbf04
cyy <cyyever@outlook.com>
2022-11-24 10:55:30 +0800

    fix code

--- fuzz_test/read_fuzzing.cpp
+++ fuzz_test/read_fuzzing.cpp
@@ -7,11 +7,11 @@
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size);
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   try {
+    bool o_create = true;
+    bool o_excl = false;
     if (Size < 100) {
       return 0;
     }
-    bool o_create = true;
-    bool o_excl = false;
     std::string path(reinterpret_cast<const char *>(Data), Size / 2);
     Data += Size / 2;
     Size -= Size / 2;
@@ -19,13 +19,18 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     if (!open_res.has_value()) {
       return 0;
     }
+    auto fd = open_res.value().first;
     uint64_t offset = *reinterpret_cast<const uint64_t *>(Data);
     Data += sizeof(uint64_t);
     Size -= sizeof(uint64_t);
-    auto fd = open_res.value().first;
+    uint64_t length = *reinterpret_cast<const uint64_t *>(Data);
+    Data += sizeof(uint64_t);
+    Size -= sizeof(uint64_t);
+    get_file_system_impl().read(fd, {offset, length});
     raid_fs::const_block_data_view_type data_view(
         reinterpret_cast<const char *>(Data), Size);
     get_file_system_impl().write(fd, offset, data_view);
+    get_file_system_impl().read(fd, {offset, length});
   } catch (const std::exception &) {
   }
   return 0; // Non-zero return values are reserved for future use.
--- fuzz_test/remove_fuzzing.cpp
+++ fuzz_test/remove_fuzzing.cpp
@@ -7,16 +7,7 @@
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size);
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   try {
-    bool o_create = true;
-    bool o_excl = true;
-    if (Size >= 2) {
-      o_create = Data[0];
-      o_excl = Data[1];
-      Data += 2;
-      Size -= 2;
-    }
     std::string path(reinterpret_cast<const char *>(Data), Size);
-    get_file_system_impl().open(path, o_create, o_excl);
     get_file_system_impl().remove_file(path);
     get_file_system_impl().remove_dir(path);
   } catch (const std::exception &) {
